2f0ecf19eadd59c5b3a8b68abbd7b5911930fabe
Paul-Antoine <paul-antoine.chevalier@data.gouv.fr>
2019-02-05 17:57:05 +0100

    ajout de la part des contrôles par mois

--- main.R
+++ main.R
@@ -17,13 +17,32 @@ table_count_satisfaction <- list.files(path = "data-raw/", pattern = "export\\_a
   map_df("result")
 write_csv(x = table_count_satisfaction, "data-raw/table_count_satisfaction.csv")
 
+
+table_count_satisfaction <- read_csv("data-raw/table_count_satisfaction.csv")
+
 table_count_satisfaction %>% 
   group_by(Synthese_eval_sanit) %>%
   summarise(n = sum(n)) %>%
   mutate(share = 100 * n / sum(n))
 
     
+table_count_satisfaction %>% 
+  glimpse()
 
+table_count_satisfaction$Synthese_eval_sanit %>% factor() %>% levels()
+  
+table_count_satisfaction %>%
+  mutate(
+    date = ymd(date), 
+    synthese = factor(
+      x = Synthese_eval_sanit, 
+      levels = c("A corriger de manière urgente", "A améliorer", "Satisfaisant", "Très satisfaisant")
+      )
+  ) 
+  ggplot() + 
+  geom_point(mapping = aes(x = date, y = n)) + 
+  scale_x_date() + 
+  scale_y_continuous()
 
 
 
